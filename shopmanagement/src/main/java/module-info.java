e21361c0076c3a97a980e5e6e81214fb0dd07e42
TheftJord <voltstar78@gmail.com>
2025-05-22 17:52:10 -0400

    quick push

--- shopmanagement/src/main/java/module-info.java
+++ shopmanagement/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.management {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-    opens com.management to javafx.fxml;
+    opens com.management to javafx.fxml, com.google.gson;
     exports com.management;
 }
